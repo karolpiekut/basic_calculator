a9c86282fd26676651bd7f85a9d477cad8d4f7bb
kpiekut <karol.piekut@pm.me>
2023-02-09 20:21:28 +0000

    add operator functionality when no = pressed

--- scripts.js
+++ scripts.js
@@ -24,13 +24,9 @@ function deduct(...par) {
     );
 }
 
-function percentage(par1) {
-    return par1 / 100;
-}
-
 //operate will take three arguments, first number, function, second number and limit the result to 9 digits
-function operate(par1, fun, par2) {
-    return fun(par1, par2);
+function operate(par1, par2, fun) {
+    return fun(parseFloat(par1), parseFloat(par2));
 }
 
 // event listeners and query selectors
@@ -47,69 +43,159 @@ Array.from(buttons).forEach(button =>
 //global variables
 
 let value = "0";
-let valuesArr = [];
+let firstValue = "0";
+let operator;
+let grandTotal;
 currentValueDisplay.innerText = 0;
-
 
 function calculator(selectedButton) {
     let selected = selectedButton.target.value;
-    // won't allow to add second comma in decimals
-    if (selected === "." && value.includes(".") === true) {
+    // clear button functionality
+    if (selected === "C") {
+        value = "0";
+        firstValue = "0";
+        operator = undefined;
+        grandTotal = undefined;
+        console.log(value, firstValue, operator, grandTotal);
+    } // won't allow to add second comma in decimals
+    else if (selected === "." && value.includes(".") === true) {
         dot.disabled = true;
-    } //won't remove initial 0 from the value
+    } //if delete pressed, won't delete initial 0
     else if (selected === "del" && value === "0") {
         value = "0";
-    } //delete button will work
+        console.log(value);
+    } //delete button functionality
     else if (selected === "del" && value !== "0") {
         value = value.substring(0, value.length - 1);
         console.log(value);
-    } //if number or dot pressed, will concatenate selection with a global variable value
+    } //percentage button functionality
+    else if (selected === "%") {
+        value = (value / 100).toString();
+        console.log(value);
+        console.log(`percentageValue: ${value}`);
+    } //plus/minus button functionality
+    else if (selected === "+/-") {
+        if (Math.sign(parseFloat(value)) === 1) {
+            value = (-Math.abs(value)).toString()
+            console.log(value);
+        } else if (Math.sign(parseFloat(value)) === -1) {
+            value = Math.abs(value).toString();
+            console.log(value);
+        }
+    }//if number or dot pressed, will concatenate selection with a global variable value
     else if (Number.isNaN(parseFloat(selected)) === false || selected === ".") {
         value += selected;
         console.log(value);
-    } else if (selected !== "=" || selected !== "del") { //if operation button selected, push first value into array
-        valuesArr.push(parseFloat(value)); //push first value into array
-        value = 0; //sets value back to 0
-        console.log(`valuesArr: ${valuesArr}`);
+    } else if (selected === "+" && operator === undefined) {
+        firstValue = value;
+        value = "0";
+        operator = add;
+        console.log(`firstValueAfterPlus: ${firstValue}`);
+        console.log(`valueAfterPlus: ${value}`);
+        console.log(`operatorAfterPlus: ${operator}`);
+    } else if (selected === "+" && operator === add) {
+        grandTotal = operate(firstValue, value, operator);
+        console.log(`operator after equals: ${operator}`);
+        console.log(`Grand Total After Equals: ${grandTotal}`);
+        console.log(`value after equals: ${value}`);
+        console.log(`calc value 1 after equals ${firstValue}`);
+    } else if (selected === "+" && operator !== add && operator !== undefined) {
+        operator = add;
+        console.log(operator);
+    }else if (selected === "-" && operator === undefined) {
+        firstValue = value;
+        value = "0";
+        operator = deduct;
+        console.log(`firstValueAfterminus: ${firstValue}`);
+        console.log(`valueAfterminus: ${value}`);
+        console.log(`operatorAfterminus: ${operator}`);
+    } else if (selected === "-" && operator === deduct) {
+        grandTotal = operate(firstValue, value, operator);
+        console.log(`operator after equals: ${operator}`);
+        console.log(`Grand Total After Equals: ${grandTotal}`);
+        console.log(`value after equals: ${value}`);
+        console.log(`calc value 1 after equals ${firstValue}`);
+    } else if (selected === "-" && operator !== deduct && operator !== undefined) {
+        operator = deduct;
+        console.log(operator);
+    }else if (selected === "÷" && operator === undefined) {
+        firstValue = value;
+        value = "0";
+        operator = divide;
+        console.log(`firstValueAfterdivide: ${firstValue}`);
+        console.log(`valueAfterdivide: ${value}`);
+        console.log(`operatorAfterdivide: ${operator}`);
+    } else if (selected === "÷" && operator === divide) {
+        grandTotal = operate(firstValue, value, operator);
+        console.log(`operator after equals: ${operator}`);
+        console.log(`Grand Total After Equals: ${grandTotal}`);
+        console.log(`value after equals: ${value}`);
+        console.log(`calc value 1 after equals ${firstValue}`);
+    } else if (selected === "÷" && operator !== divide && operator !== undefined) {
+        operator = divide;
+        console.log(operator);
+    }else if (selected === "×" && operator === undefined) {
+        firstValue = value;
+        value = "0";
+        operator = multiply;
+        console.log(`firstValueAftermulti: ${firstValue}`);
+        console.log(`valueAftermulit: ${value}`);
+        console.log(`operatorAftermulti: ${operator}`);
+    } else if (selected === "×" && operator === multiply) {
+        grandTotal = operate(firstValue, value, operator);
+        console.log(`operator after equals: ${operator}`);
+        console.log(`Grand Total After Equals: ${grandTotal}`);
+        console.log(`value after equals: ${value}`);
+        console.log(`calc value 1 after equals ${firstValue}`);
+    } else if (selected === "×" && operator !== multiply && operator !== undefined) {
+        operator = multiply;
+        console.log(operator);
     }
-
-
-
-    if (selected === "+") {
-        valuesArr.push(add);
-        console.log(value);
-        console.log(`with plus: ${valuesArr}`)
+    else if (selected === "=" && grandTotal === undefined) {
+        grandTotal = operate(firstValue, value, operator);
+        console.log(`operator after equals: ${operator}`);
+        console.log(`Grand Total After Equals: ${grandTotal}`);
+        console.log(`value after equals: ${value}`);
+        console.log(`calc value 1 after equals ${firstValue}`);
+    } else if (selected === "=" && grandTotal !== undefined) {
+        grandTotal = operate(grandTotal, value, operator);
+        console.log(`more equals: ${grandTotal}`);
     }
-
-
-//
 
 }
 
+//NEXT STEP - ADD FUNCTIONALITY WHERE GRAND TOTAL IS PRESENT AND NEED TO TAKE INTO ACCOUNT WHAT IT IS WITH CALCULATIONS
+//IF GRAND TOTAL IS NOT UNDEFINED THEN EXECUTE OPERATE WHERE PAR1 IS GRAND TOTAL IF
+
+    /*
 
 
-//HOW TO MAKE SURE THAT THE ARRAY IS GOING TO BE POPULATED IN THE RIGHT ORDER
+        REPEATED EQUALS SHOULD RE-RUN LAST CALCULATION ADDING TO GRAND TOTAL
 
-//push function button into valuesArr
-/*
-
-    if (selected === "+") {
-        valuesArr.push(add);
-    } else if (selected === "-") {
-        valuesArr.push(deduct);
-    } else if (selected === "×") {
-        valuesArr.push(multiply);
-    } else if (selected === "÷") {
-        valuesArr.push(divide);
-    } else if (selected === "%") {
-        valuesArr.push(percentage);
-    } else if (selected === "c") { //reset calculator
-        value = 0;
-        valuesArr = [];
-    }
+        pick a number once you are happy with the selection select operator
+        operator to be stored in operator variable
+        store value in firstValue variable
+        set value to "0"
+        if a different operator is chosen, overwrite operator existing operator, do not touch firstValue
+        pick a number, once you are happy with the selection press equals,
+        if you firstValue, operator and value are populated and another operator is selected, function from the operator
+        variable should get executed as per equals
+        if another operator is selected instead of equals, get firstValue populated with GrandTotal value and second
+        selection to be populated with 
+        this will call a function based on operator value
+        this will populate grandTotal with the result
 
 
-}
+        and set value, calc value and operator to nil
+
+
+
+        Your calculator should not evaluate more than a single pair of numbers at a time.
+        Example: you press a number button (12), followed by an operator button (+), a second number button (7),
+        and finally a second operator button (-). Your calculator should then do the following: first,
+        evaluate the first pair of numbers (12 + 7), second, display the result of that calculation (19),
+        and finally, use that result (19) as the first number in your new calculation, along with the next operator (-).
+
 
 
 if (value === 0) {
@@ -122,29 +208,4 @@ if (value === 0) {
     currentValueDisplay.innerText = parseFloat(value); //display value without a leading 0
 }
 
-/*
-
-
-    
-
-    //need logic when equals will only execute if valuesArr.length === 2;
-
-    if (select === "=") {
-        valuesArr.push(parseFloat(value));
-        evaluation = operate(valuesArr[0], valuesArr[1], valuesArr[2]);
-        valuesArr = [];
-        valuesArr.push(evaluation);
-        value = 0;
-    }
-
-
-
-    //display logic
-
-}
-
-
 */
-
-
-
